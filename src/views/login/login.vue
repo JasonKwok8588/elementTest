14183856e26579301e6f69a9e7b8dc88f701e2bb
kwok8854 <92763918@qq.com>
2020-03-03 18:36:31 +0800

    new project

---
+++ src/views/login/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="login-wapper">
+    <el-form
+      :model="ruleForm"
+      status-icon
+      :rules="rules"
+      ref="ruleForm"
+      class="demo-ruleForm"
+    >
+      <el-form-item  prop="username">
+        <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item  prop="pwd">
+        <el-input v-model.number="ruleForm.age"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
